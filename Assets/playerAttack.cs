6b38db2fb1eb6d3388991a37a2f8fd8aa514cbdb
Kacper-with-a-K <158076660+Kacper-with-a-K@users.noreply.github.com>
2024-11-08 12:21:26 +0000

    attack animations

--- Assets/playerAttack.cs
+++ Assets/playerAttack.cs
@@ -5,29 +5,27 @@ using UnityEngine;
 
 public class playerAttack : MonoBehaviour
 {
-    [Header("Light combo")]
-    public GameObject attack1;
-    public GameObject attack2;
-    public GameObject attack3;
 
+    public Animator Attack;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Attack = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+
+
+        if (Input.GetMouseButtonDown(0))
         {
-            attack1.SetActive(true);
-            StartCoroutine(StopAttack(0.5f));        }
+            Attack.SetTrigger("Attack");
+        }
     }
 
-    IEnumerator StopAttack(float time)
-    {
-        yield return new WaitForSeconds(time);
-        attack1.SetActive(false);
-    }
+   
+
+   
+
 }
